8421efc36a20fbbee6d5a6d87b04d5a0d5ac3e27
gameplan1975 <gameplan1975@gmail.com>
2020-04-08 05:20:02 +0000

    ログイン機能実装途中＿メンター確認用

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  get "/" => "homes#top"  
  get "/homes/help" => "homes#help"
- resources :users
+ resources :users, only: [:new, :create, :show]
  resources :blogs do
     collection do
       post :confirm
